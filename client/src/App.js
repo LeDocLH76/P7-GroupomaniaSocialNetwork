55234906f16310c6937aafb3dce664c839510dc5
LeDoc <ledoclh76fr@gmail.com>
2022-06-13 13:04:24 +0200

    Create react + router + blabla dans le router

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import { BrowserRouter, Link, Navigate, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Main from './pages/Main';
+import { useState } from 'react';
+
+function App() {
+   const [isAuth] = useState(false);
+   return (
+      <div className="App">
+         <BrowserRouter>
+            <nav>
+               <h1>Groupotruc</h1>
+               <Link to="/">Home</Link>
+               <Link to="/signup">Signup</Link>
+               <Link to="/login">Login</Link>
+            </nav>
+            <Routes>
+               <Route path="/" element={<Home />} />
+               <Route path="/signup" element={<Signup />} />
+               <Route path="/login" element={<Login />} />
+               <Route path="/main" element={isAuth ? <Main /> : <Navigate to="/login" />} />
+               <Route path="/*" element={<Navigate to="/" />} />
+            </Routes>
+         </BrowserRouter>
+      </div>
+   );
+}
+
+export default App;
